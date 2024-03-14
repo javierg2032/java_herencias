6feded1f2fedac52f86b19465d8a32d3a550bafc
Javier Galán Cortés <javier.galan2032@gmail.com>
2024-03-14 12:15:30 +0100

    Versión 1

--- Instrumento.java
+++ Instrumento.java
@@ -14,8 +14,21 @@ public abstract class Instrumento {// EJERCICIO_6_04
 
 	// METODOS DE LA CLASE
 	public void add(String nota) {
+
+		for (int i = 0; i < NotasMusicales.values().length; i++) { // EL METODO VALUES DEVUELVE EL ARRAY DE LOS ENUMS
+			if (NotasMusicales.values()[i].equals(NotasMusicales.valueOf(nota))) {
+				melodia = Arrays.copyOf(melodia, melodia.length + 1);
+				melodia[melodia.length - 1] = NotasMusicales.valueOf(nota);
+			}
+		}
+
+	}
+
+	public void addEnum(NotasMusicales nota) {
+
 		melodia = Arrays.copyOf(melodia, melodia.length + 1);
-		melodia[melodia.length - 1] = NotasMusicales.valueOf(nota);
+		melodia[melodia.length - 1] = nota;
+
 	}
 
 	public abstract void interpretar();
