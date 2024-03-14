56b1c7085d684a317851e947ec0fa634e5bd178e
Javier Galán Cortés <javier.galan2032@gmail.com>
2024-03-14 12:13:15 +0100

    Main
    Creación y comprobación de los métodos en el main

---
+++ Ejercicio_6_01.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio_6_01 {
+	public static void main(String[] args) {
+		/*Hora hora1 = new Hora(10, 44);
+		System.out.println(hora1.toString());
+
+		Hora hora2 = new Hora(22, 59);
+		hora2.inc();
+		System.out.println(hora2);
+		System.out.println(hora1.setMinutos(5));
+		System.out.println(hora1);*/
+		
+		HoraExacta hora1=new HoraExacta(10,59,1);
+		System.out.println(hora1.toString());
+	}
+}
---
+++ Ejercicio_6_04.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio_6_04 {
+
+	public static void main(String[] args) {
+
+		// OPCION 1
+		NotasMusicales notas1[] = new NotasMusicales[2];
+		notas1[0] = NotasMusicales.DO;
+		notas1[1] = NotasMusicales.SOL;
+
+		// OPCION 2
+		NotasMusicales notas2[] = { NotasMusicales.DO, NotasMusicales.SOL };
+
+		Piano piano1 = new Piano(notas1);
+		piano1.interpretar();
+		piano1.add("RE");
+		piano1.interpretar();
+	}
+
+}
